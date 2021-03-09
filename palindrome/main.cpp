34689e3ec26c35e8bda0bea7c43b0c20cc3ebb15
Hongyi Zhou <hongyizh@andrew.cmu.edu>
2021-03-09 22:06:30 +0800

    Initial Commit

---
+++ palindrome/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  palindrome
+//
+//  Created by Hongyi Zhou on 3/9/21.
+//  Copyright © 2021 Hongyi Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
